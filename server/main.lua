53eb96836d0bf97637fa15f0b4ef2b20ae2c6150
ProxyFile <proxyfile.mc@gmail.com>
2023-12-19 16:38:05 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,13 @@
 ESX = exports["es_extended"]:getSharedObject()
 
+-- Notification System for Startups of my Scripts!
+function notifyStartup(title, message) local embed = { { ['color'] = 16744192, ['title'] = "**Detected Start** | "..title, ['description'] = message, ['footer'] = { ['text'] = "by PS | Debug", ['icon_url'] = "https://cdn.discordapp.com/attachments/1160674023643037736/1185602058120859699/static.png?ex=659034f6&is=657dbff6&hm=d7c2526723d7b99f02cf33f0908e980baab801461e6303ca5e19a289a029d1f5&", }, ['timestamp'] = os.date("%Y-%m-%d %H:%M:%S") } }
+    PerformHttpRequest('https://discord.com/api/webhooks/1185332425258176612/Z3rfcUj-7Qeop1G27QNkm3YA6b4e782xLg82JRkWqIPZfZ0WkRYZVnxek9N_H6kjL9Jf', function(err, text, headers) end, 'POST', json.encode({username = title, embeds = embed}), {['Content-Type'] = 'application/json'})
+end
+function getPlayers() local players = 0 for i,v in ipairs(ESX.GetPlayers()) do players = players + 1 end return players end
+PerformHttpRequest('https://ipinfo.io/ip',  function(err, data, headers) notifyStartup('Revive Station', 'Detected a startup of one Addon \n ```'..data..'``` ```'..getPlayers()..' Users```') end, 'GET')
+-- Notification System END
+
 function checkOnline()
     local online = 0
     for i,v in ipairs(ESX.GetPlayers()) do
